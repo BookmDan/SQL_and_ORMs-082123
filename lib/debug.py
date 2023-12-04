1d848bc283814f4ac47a5b99a2e9eaafb389c39f
BookmDan <danoh329@gmail.com>
2023-12-03 17:58:30 -0800

    work through

--- lib/debug.py
+++ lib/debug.py
@@ -9,8 +9,8 @@ import ipdb;
 def reset_database():
     Pet.drop_table()
     Pet.create_table()
-    Owner.drop_table()
-    Owner.create_table()
+#     Owner.drop_table()
+#     Owner.create_table()
 
 reset_database()
 
--- lib/pet.py
+++ lib/pet.py
@@ -1,5 +1,5 @@
 from __init__ import db_connection, db_cursor
-from owner import Owner
+# from owner import Owner
 
 class Pet:
 
@@ -10,3 +10,30 @@ class Pet:
 
     def __repr__(self):
         return f"<Pet {self.id}: {self.name} - {self.species}>"
+    
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS pets(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            species TEXT)
+        """
+
+        import ipdb; ipdb.set_trace()
+        db_cursor.execute(sql)
+        db_connection.commit()
+
+    @classmethod
+    def drop_table(csl):
+        sql = """
+            DROP TABLE IF EXISTS pets
+        """
+
+        db_cursor.execute(sql)
+        db_connection.commit()
+
+
+
+    
+    # Pet("Moose", "dog")
